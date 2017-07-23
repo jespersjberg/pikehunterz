0ac6a48587097c8baff4e6aa79863efd0da3e991
Jesper Sjöberg <jespersjoberg@Jespers-MacBook-Pro.local>
2017-07-23 21:27:55 +0200

    First commit

--- windUp/FirstViewController.swift
+++ windUp/FirstViewController.swift
@@ -10,11 +10,55 @@ import UIKit
 
 class FirstViewController: UIViewController {
 
+    @IBOutlet weak var compassPin: UIImageView!
+    @IBAction func btn(_ sender: Any) {
+        compassPin.transform = compassPin.transform.rotated(by: CGFloat(M_PI_4))
+
+        
+    }
+    
+    
+    @IBOutlet weak var windLbl: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
+     
+        
+        let urlString = "http://opendata-download-metanalys.smhi.se/api/category/mesan1g/version/1/geotype/point/lon/20/lat/62/data.json"
+        let url = URL(string: urlString)
+        let task = URLSession.shared.dataTask(with: url!) { (data, response, error) in
+            if error != nil{
+                print("ERROR task")
+            } else{
+                
+                if let content = data {
+                
+                    do {
+                        let json = try JSONSerialization.jsonObject(with: content, options: JSONSerialization.ReadingOptions.mutableContainers) as AnyObject
+                        //print(json)
+                        
+                        if let timeSeries = json["timeSeries"] as? NSArray {
+                            print(timeSeries[0])
+                        }
+                    }
+                    
+                    catch{
+                        
+                    }
+                }
+            }
+            
+        }
+        task.resume()
+        
+   
+        
+        
+        
+    }//ViewDidLoad
 
+ 
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
